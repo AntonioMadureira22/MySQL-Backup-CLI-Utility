2c095b17268480368ceff2f0f97ce30c54f48d3d
AntonioMadureira22 <you@example.com>
2024-08-27 16:23:57 -0400

    building togther the program

--- index.js
+++ index.js
@@ -2,3 +2,16 @@ const { exec } = require('child_process');
 const mysql = require('mysql2');
 const { Command } = require('commander');
 require('dotenv').config();
+
+const program = new Command();
+
+program
+.version('1.0.0')
+.description('MySQL Backup CLI Utility')
+.option('-d, --database <name>', 'Database name')
+.option('-u, --user <name>', 'Database user', process.env.DB_USER) // to pull the username 
+.option('-p, --password <password>', 'Database password', process.env.DB_PASSWORD)
+.option('-h, --host <host>', 'Database host', process.env.DB_HOST || 'localhost')
+.option('-o, --output <file>', 'Output file', `back_${Date.now().sql}`)
+
+const options = program.opts();
