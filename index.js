4162ebfa98f7ccc6c0ce2ec60725d4a994aded93
AntonioMadureira22 <you@example.com>
2024-08-29 17:06:48 -0400

    finished the script to allow the backup

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { exec } = require('child_process');
 const mysql = require('mysql2');
 const { Command } = require('commander');
+const { stdout, stderr } = require('process');
 require('dotenv').config();
 
 const program = new Command();
@@ -15,3 +16,19 @@ program
 .option('-o, --output <file>', 'Output file', `back_${Date.now().sql}`)
 
 const options = program.opts();
+
+//reminder
+if(!options.database){
+    console.error('Database name is required');
+    process.exit(1);
+}
+
+const backupCommand = `mysqldump -u ${options.user} -p ${options.password} -h ${options.host} ${options.database} > ${options.output}`;
+
+exec(backupCommand, (error, stdout, stderr) => {
+    if(error) {
+        console.log(`Error executing backup: ${error.message}`);
+        return;
+    }
+    console.log(`Backup completed successfully!: ${options.output}`);
+});
